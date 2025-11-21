965181cd58da43f5f15caa974a71193d7a5c84b3
lochlaneaton <125938235+lochlaneaton@users.noreply.github.com>
2025-11-21 12:50:54 -0600

    Navigation logic for profile page and watchlist implemented.

---
+++ pages/watched.tsx
@@ -0,0 +1,47 @@
+import { useRouter } from "next/router";
+import { Menubar, MenubarItem } from "@/components/ui/menubar"
+import { MenubarMenu, MenubarTrigger } from "@radix-ui/react-menubar";
+import { motion } from 'framer-motion';
+export default function watchedMovies(){
+
+    const router = useRouter(); 
+    return(
+        <div className="flex justify-center">
+            {/* Include Profile Navigation Bar */}
+                <Menubar className="mx-auto flex gap-4">
+                    <MenubarMenu>
+                        <MenubarTrigger 
+                        className="hover:bg-primary hover:text-black rounded-md"
+                        onClick={() => router.push("/profile-page")}>
+                            Profile
+                        </MenubarTrigger>
+                    </MenubarMenu>
+                    <MenubarMenu>
+                        <MenubarTrigger 
+                        className="hover:bg-primary hover:text-black rounded-md"
+                        onClick={() => router.push("/watched")}>
+                            Watched
+                        </MenubarTrigger>
+                    </MenubarMenu>
+                    <MenubarMenu>
+                        <MenubarTrigger 
+                        className="hover:bg-primary hover:text-black rounded-md"
+                        onClick={() => router.push("/watch-queue")}>
+                            Movie Queue
+                        </MenubarTrigger>
+                    </MenubarMenu>
+                </Menubar>
+            <div>
+            <motion.div
+            initial={{x: "100%"}}
+            animate={{x: 0}}
+            exit={{x: "-100%"}}
+            transition={{duration: 0.5}}
+            >
+               {/* Page content goes here */}
+                
+            </motion.div>
+            </div>
+        </div>
+    )
+}
